3443fe99954232a76721654a350d6fa8a44227e7
angelamyungjoosong <angelaforever122@gmail.com>
2023-06-21 12:26:00 +0900

    과제 완성

---
+++ src/main/java/com/example/study_servlet/commons/Common.java
@@ -0,0 +1,10 @@
+package com.example.study_servlet.commons;
+import java.util.UUID;
+
+public class Common {
+    public String generateUUID() {
+        return UUID.randomUUID().toString();
+}
+}
+
+
