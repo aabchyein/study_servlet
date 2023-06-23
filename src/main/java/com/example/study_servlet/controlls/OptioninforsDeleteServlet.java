69bf2f354c1429728c0b33ad1afe20a572e08f23
aabchyein <abcqaz369@naver.com>
2023-06-23 09:31:09 +0900

    26-06-23 혜인 첫번째

--- src/main/java/com/example/study_servlet/controlls/OptioninforsDeleteServlet.java
+++ src/main/java/com/example/study_servlet/controlls/OptioninforsDeleteServlet.java
@@ -11,23 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.example.study_servlet.daos.OptionInforsDao;
 
-@WebServlet(urlPatterns = "/optioninforsDeleteServlet")
-public class OptioninforsDeleteServlet extends HttpServlet{
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            String unique_id = request.getParameter("unique_id");
-            
-            OptionInforsDao optionInforsDao = new OptionInforsDao();
-            int count = optionInforsDao.DeleteWithUniqueID(unique_id);
+@WebServlet(urlPatterns = "/optionInforsDeleteServlet")
+public class OptionInforsDeleteServlet extends HttpServlet {
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       try {
+        String unique_id = request.getParameter("unique_id");
 
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter printWriter = response.getWriter();
-            String contents = "Delete count :"+count;
-            printWriter.println(contents);
-            printWriter.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
+        OptionInforsDao optionInforsDao = new OptionInforsDao();
+        int count = optionInforsDao.DeleteWithUniqueID(unique_id);
+
+
+       response.setContentType("text/html;charset=UTF-8");
+       PrintWriter printWriter = response.getWriter();
+       String contents = "Delete count: " +count;
+       
+       printWriter.println(contents);
+       printWriter.close();
+            
+       } catch (Exception e) {
+        System.out.println(e.getMessage());
+
+       }
+   }
 }
