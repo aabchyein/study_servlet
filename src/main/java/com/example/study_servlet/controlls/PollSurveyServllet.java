f1926504742495f73d4354ea093c19f2d1d87ec3
aabchyein <abcqaz369@naver.com>
2023-06-29 14:24:20 +0900

    23-06-29

--- src/main/java/com/example/study_servlet/controlls/PollSurveyServllet.java
+++ src/main/java/com/example/study_servlet/controlls/PollSurveyServllet.java
@@ -13,32 +13,36 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.example.study_servlet.daos.PollsDao;
 
-@WebServlet(urlPatterns = "/poll/SurveyServllet")
-public class PollSurveyServllet extends HttpServlet{
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+@WebServlet(urlPatterns = "/poll/SurveyServlet")
+public class PollSurveyServllet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            // String contents = "Yoju Lab !";
+
             String contents = request.getParameter("company");
 
             PollsDao pollsDao = new PollsDao();
-            ArrayList surveyList = pollsDao.SelectWithSearch(contents); // surveyList는 내부에서 사용할 변수를 정해준 것.
-            for(int i=0; i < surveyList.size(); i = i+3) {
-                HashMap survey = (HashMap) surveyList.get(i); //List를 Hashmap으로 먼저 풀어줌
-                String questions = (String) survey.get("QUESTIONS");// Hashmap을 풀어준다.
-                System.out.println(questions);
-                for(int e=0; e <3; e = e+1) {
-                    survey = (HashMap) surveyList.get(e);
-                    String choice =(String) survey.get("CHOICE");
+            ArrayList surveyList = pollsDao.SelectWithSearch(contents);
+           
+
+             String compare = "";
+             for (int i = 0; i < surveyList.size(); i = i + 1) {
+                HashMap survey = (HashMap) surveyList.get(i);
+                String questions = (String) survey.get("QUESTIONS");
+                String questionsId = (String) survey.get("QUESTIONS_ID");
+                String choice = (String) survey.get("CHOICE");
+                if (!compare.equals(questionsId)) {
+                    System.out.println(questions);
+                    System.out.println(choice);
+                    compare = questionsId;
+                } else {
                     System.out.println(choice);
                 }
-                
-                
             }
-
-            request.setAttribute("contents", contents);
+request.setAttribute("contents", contents);
+            request.setAttribute("surveyList", surveyList);
             // 다음 파일 호출
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/surveys/survey.jsp"); // 브라우저에 띄웠던 경로를 적어줘야 함.
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/surveys/survey.jsp");
             requestDispatcher.forward(request, response);
 
         } catch (Exception e) {
@@ -46,9 +50,3 @@ public class PollSurveyServllet extends HttpServlet{
         }
     }
 }
-
-
-// html을 만들어서 확인하고 jsp만들어서 브라우저에서 작동되는 것 확인
-// view를 먼저 띄워보고 servlet으로 jsp 만드는 것을 해결
-// servlet을 만들면 servlet의 url로 호출을 해야 jsp가 출력이 된다.
-// dispatcher 된다는 것은 jsp로 넘어간다는 것을 의미함
